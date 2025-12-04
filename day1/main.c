2364445eade5a14f2331206d8f07af1cbef6388d
Rhys Povey <rhys.povey@capgemini.com>
2025-12-04 16:11:03 +0000

    day1 2nd task finished

--- day1/main.c
+++ day1/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
 int handlePrefix(char s[]) {
 	return (-2 * (s[0] == 'L')) + 1;
@@ -18,18 +19,37 @@ int handleNum(char s[]) {
 	return v;
 }
 
-int main() {
+int main(int argc, char *argv[]) {
 	FILE *f;
-	f = fopen("input.txt", "r");
+	f = fopen(argv[1], "r");
 
 	char buff[50];
 	int dial = 50;
-	int numZeros = 0;
+	int numXZero = 0;
+	int toRotate;
+	int prevdial = 50;
+	int diff;
 	while (fgets(buff, 50, f)) {
+		toRotate = (handlePrefix(buff) * handleNum(buff));
+		dial = (dial + toRotate) % 100;
+		if (dial < 0) {dial = 100 + dial;}
+		printf("Dial: %d After rotation: %d From: %d\n", dial, toRotate, prevdial);
+		if (prevdial + toRotate < 0 || prevdial + toRotate > 100 || prevdial == 0) {
+			diff = toRotate % 100;
+			numXZero += (prevdial + diff < 0 || prevdial + diff > 100 || prevdial == 0) && (diff != 0);
+			numXZero += floor(abs(toRotate / 100));
+			printf("Incremented numXZero by %f\n", 1 + floor(abs(toRotate / 100)));
+		}
+		/*
 		dial = (dial + (handlePrefix(buff) * handleNum(buff))) % 100;
+		printf("%d\n", dial);
 		numZeros += dial == 0;
+		numXZero += dial * prevdial < 0 || dial == 0;
+		prevdial = dial; 
+		*/
+		prevdial = dial;
     };
-	printf("Number of zeros: %d", numZeros);
+	printf("Times zero was crossed: %d\n", numXZero);
 	fclose(f);
 	return 0;
 }
